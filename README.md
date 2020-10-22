d5cc360348fd1ad7d0a9b5147db02453ad88a5d0
Han <bin315a1@gmail.com>
2020-10-22 15:41:33 -0700

    input data partitioning per state

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Course project for UCLA CS145, Introduction to Data Mining
 
-hi
+## Initializing Input Data
 
-ehy!!!!
+### Partitioning daily report data by states
 
-what's up!!@#\$!@
+To transform input data, run:
+
+> python transform_input.py
+
+It will then create a csv file for each states, each containing its state's daily report. Miscellaneous states from the input data set are ignored
+
+**NOTE** Each time this script is ran, all the `<state>.csv` files are truncated an refilled from the daily report files.
--- transform_input.py
+++ transform_input.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
+import math
 import os.path
 
 DAYS_COUNT = {4: 30, 5: 31, 6: 30, 7: 31, 8: 31}
 PER_STATE_DATA_PATH = './data/daily_report_per_states/'
+KEPT_FIELDS = ["Confirmed", "Deaths", "Recovered", "Active", "FIPS", "Incident_Rate", "People_Tested",
+               "People_Hospitalized", "Mortality_Rate", "UID", "ISO3", "Testing_Rate", "Hospitalization_Rate"]
 
 
 def file_exists(path):
@@ -12,9 +15,9 @@ def file_exists(path):
     return False
 
 
-def truncate_all_state_files():
-    states = pd.read_csv(
-        './data/daily_report_per_states/states.csv', engine="python")
+def reset_all_state_files():
+    states_file = PER_STATE_DATA_PATH + 'states/states.csv'
+    states = pd.read_csv(states_file, engine="python")
     for index, row in states.iterrows():
         state = row.loc['State']
         path = PER_STATE_DATA_PATH + state + ".csv"
@@ -22,6 +25,11 @@ def truncate_all_state_files():
             open(path, "x")
         file = open(path, "w")
         file.truncate()
+        init_row = "Date"
+        for field in KEPT_FIELDS:
+            init_row += "," + field
+        init_row += "\n"
+        file.write(init_row)
 
 
 def day_str(date):
@@ -32,29 +40,38 @@ def day_str(date):
 # partition input data by state
 
 
-def partition_state(data):
+def partition_state(data, date):
     for index, row in data.iterrows():
         state_csv_file = PER_STATE_DATA_PATH + \
             row.loc["Province_State"] + ".csv"
         if (not file_exists(state_csv_file)):
             continue
-        for
-        print(row.values)
-
-# append csv files
+        csv_row = date
+        for field in row.values[5:]:
+            if (isinstance(field, str)):
+                csv_row = csv_row + ',' + field
+            elif (math.isnan(field)):
+                csv_row = csv_row + ','
+            else:
+                csv_row = csv_row + ',' + str(field)
+        csv_row = csv_row + "\n"
+        with open(state_csv_file, 'a') as fd:
+            fd.write(csv_row)
+        # append csv files
 
 
 def main():
-    truncate_all_state_files()
+    reset_all_state_files()
 
     for month in DAYS_COUNT:
         for day in range(1, DAYS_COUNT[month]+1):
-            date = "0" + str(month) + "-" + day_str(day) + "-2020.csv"
-            path = './data/daily_report/' + date
-            if(not file_exists(path)):
+            date = "0" + str(month) + "-" + day_str(day)
+            filename = date + "-2020.csv"
+            file_path = './data/daily_report/' + filename
+            if(not file_exists(file_path)):
                 continue
-            daily_report_data = pd.read_csv(path, engine="python")
-            partition_state(daily_report_data)
+            daily_report_data = pd.read_csv(file_path, engine="python")
+            partition_state(daily_report_data, date)
             # add methods here
 
 
